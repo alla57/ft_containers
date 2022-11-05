73683369acfd39650d5f1703d9e73850a21f305f
alla57 <alla@LAPTOP-TA5UI7QJ.localdomain>
2022-11-05 11:50:10 +0100

    vector function members prototypes added

--- Vector.hpp
+++ Vector.hpp
@@ -3,12 +3,47 @@
 
 #include <memory>
 
-template<typename T, std::allocator>
+template<typename T, typename Allocator = std::allocator>
 class Vector
 {
 public :
+//		CONSTRUCTORS
 	Vector();
-	Vector();
+	explicit Vector(const Allocator& alloc);
+	explicit Vector(size_type count, const T& value = T(), const Allocator& alloc = Allocator());
+	template<class InputIt>
+	Vector(InputIt first, InputIt last, const Allocator& alloc = Allocator());
+	Vector(const Vector& other);
+//		DESTRUCTOR
+	~Vector();
+//		ASSIGNEMENT OPERATOR OVERLOAD
+	Vector& operator=(const Vector& other);
+//		MEMBER FUNCTIONS
+	void assign(size_type count, const T& value);
+	template<class InputIt>
+	void assign(InputIt first, InputIt last);
+	allocator_type get_allocator() const; // std:: ????
+//		ELEMENT ACCESS
+	reference at( size_type pos );
+	const_reference at( size_type pos ) const;
+	reference operator[]( size_type pos );
+	const_reference operator[]( size_type pos ) const;
+	reference front();
+	const_reference front() const;
+	reference back();
+	const_reference back() const;
+	T* data();
+	const T* data() const;
+//		ITERATORS
+	iterator begin();
+	const_iterator begin() const;
+	iterator end();
+	const_iterator end() const;
+	reverse_iterator rbegin();
+	const_reverse_iterator rbegin() const;
+	reverse_iterator rend();
+	const_reverse_iterator rend() const;
+//		CAPACITY
 private :
 }
 
---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+#include <iostream>
+
+int main()
+{
+	std::vector<int> b = std::vector<int> a(9);
+	std::cout << b.size() << std::endl;
+	return (0);
+}
