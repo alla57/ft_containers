1a52004ac3b4d53732a4f79aa79b896c16f39f17
Alla eddine BOUMEDIENE <alboumed@e2r3p5.clusters.42paris.fr>
2022-11-18 20:42:27 +0100

    enable_if added

---
+++ Utilities.hpp
@@ -0,0 +1,16 @@
+#ifndef UTILITIES_HPP
+# define UTILITIES_HPP
+
+namespace ft
+{
+	template<bool B, class T = void>
+	struct enable_if{};
+
+	template<class T>
+	struct enable_if<true, T>
+	{
+		typedef T type;
+	};
+}
+
+#endif
