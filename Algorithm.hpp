0293e2b87539af32a6db446617fecd4f451f3bb1
Alla eddine BOUMEDIENE <alboumed@e3r2p18.clusters.42paris.fr>
2022-11-26 17:52:01 +0100

    lexicographical compare done

--- Algorithm.hpp
+++ Algorithm.hpp
@@ -28,6 +28,36 @@ namespace ft
 		}
 		return (true);
 	}
+
+	// 			LEXICOGRAPHCAL COMPARE
+	template <class InputIT1, class InputIT2>
+	bool lexicographical_compare(InputIT1 first1, InputIT1 last1, InputIT2 first2, InputIT2 last2)
+	{
+		while (first1 != last1)
+		{
+			if (first2 == last2 || *first2 < *first1)
+				return false;
+			else if (*first1 < *first2)
+				return true;
+			++first1;
+			++first2;
+		}
+		return (first2 != last2);
+	}
+	template <class InputIT1, class InputIT2, class Compare>
+	bool lexicographical_compare(InputIT1 first1, InputIT1 last1, InputIT2 first2, InputIT2 last2, Compare comp)
+	{
+		while (first1 != last1)
+		{
+			if (first2 == last2 || comp(*first2, *first1))
+				return false;
+			else if (comp(*first1, *first2))
+				return true;
+			++first1;
+			++first2;
+		}
+		return (first2 != last2);
+	}
 }
 
 #endif
