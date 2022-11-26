55ea1a437953f30487595029eb22117087cac609
Alla eddine BOUMEDIENE <alboumed@e3r2p18.clusters.42paris.fr>
2022-11-26 17:12:56 +0100

    std::equal done

--- Algorithm.hpp
+++ Algorithm.hpp
@@ -5,37 +5,28 @@ namespace ft
 {
 	//			EQUAL
 	template<class InputIT1, class InputIT2>
-	bool equal(InputIT1 first1, InputIT1 last1, InputIT2 first2, InputIT2 last2)
+	bool equal(InputIT1 first1, InputIT1 last1, InputIT2 first2)
 	{
-		InputIT1 it1 = first1;
-		InputIT2 it2 = first2;
-		while (true)
+		while (first1 != last1)
 		{
-			if (*it1 != *it2)
+			if (*first1 != *first2)
 				return (false);
-			if (it1 == last1)
-				break;
-			++it1;
-			++it2;
+			++first1;
+			++first2;
 		}
 		return (true);
 	}
 	template<class InputIT1, class InputIT2, class BinaryPredicate>
 	bool equal(InputIT1 first1, InputIT1 last1, InputIT2 first2, BinaryPredicate pred)
 	{
-		InputIT1 i = first1;
-		if (*i != *(first2 + (i - first1)) || pred(*i, *(first2 + (i - first1))) == false)
-			return (false);
-		++i;
-		while (i != last1)
+		while (first1 != last1)
 		{
-			if (*i != *(first2 + (i - first1)) || pred(*i, *(first2 + (i - first1))) == false) // maybe only predicate condition to respect ???
+			if (!pred(*first1, *first2))
 				return (false);
-			++i;
+			++first1;
+			++first2;
 		}
-		if (!pred(first1, last1))
-			return (false);
-		equal<InputIT1, InputIT2>(first1, last1, first2, InputIT2 last2)
+		return (true);
 	}
 }
 
