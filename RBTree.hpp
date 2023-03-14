a3d1a10b103ca29b49b353381f5607657d43d547
Alla eddine BOUMEDIENE <alboumed@e3r2p18.clusters.42paris.fr>
2023-03-14 19:53:00 +0100

    dsfjlsdhflk dfksdhfkljsdhfjk

--- RBTree.hpp
+++ RBTree.hpp
@@ -23,7 +23,7 @@ namespace ft
 		typedef ft::pair<Key, Data> value_type;
 
 		// RB_Node() : color(BLACK), left(NULL), right(NULL), parent(NULL), key(){}
-		RB_Node(const value_type& value = value_type()) : color(RED), left(NULL), right(NULL), parent(NULL), key(value.first), data(value.second){}
+		RB_Node(const value_type& value = value_type()) : value(value), color(RED), left(NULL), right(NULL), parent(NULL), key(value.first){}
 
 		const RB_Node& operator=(const RB_Node& other) {
 			if (this == &other)
@@ -33,27 +33,25 @@ namespace ft
 			right = other.right;
 			parent = other.parent;
 			key = other.key;
-			data = other.data;
+			value = other.value;
 			return (*this);
 		}
 		node_ptr	parent;
 		node_ptr	left;
 		node_ptr	right;
-		const key_type	key;
-		data_type	data;
+		key_type	key;
+		value_type	value;
 		bool		color;
 	};
 	//	INCREMENT AND DECREMENT NODE
-	// template<typename Key, typename Data>
-	RB_Node*	rb_tree_increment(RB_Node* x){
-		if (x->right == NULL)
-			return x;
+	template<typename Key, typename Data>
+	RB_Node<Key, Data>*	rb_tree_increment(RB_Node<Key, Data>* x){
+		// if (x->right == NULL)
+		// 	return x;
 		if (x->right->right != NULL)
 			return minimum(x->right);
-		RB_Node* y = x->parent;
-		if (y == NULL)
-			return x;
-		while (y->right != NULL && x == y->right)
+		RB_Node<Key, Data>* y = x->parent;
+		while (x == y->right)
 		{
 			x = y;
 			y = y->parent;
@@ -61,15 +59,12 @@ namespace ft
 		return y;
 	}
 
-	RB_Node* rb_tree_decrement(RB_Node* x){
-		if (x->right == NULL)
-			return x;
+	template<typename Key, typename Data>
+	RB_Node<Key, Data>* rb_tree_decrement(RB_Node<Key, Data>* x){
 		if (x->left->left != NULL)
 			return maximum(x->left);
-		RB_Node* y = x->parent;
-		if (y == NULL)
-			return x;
-		while (y != TNULL && x == y->left)
+		RB_Node<Key, Data>* y = x->parent;
+		while (x == y->left)
 		{
 			x = y;
 			y = y->parent;
@@ -95,9 +90,14 @@ namespace ft
 		RB_Iterator() : node() {}
 		RB_Iterator(node_ptr other) : node(other) {}
 
-		reference operator*() const {return (node->data)}
-		pointer operator->() const {return(&(node->data))}
-		RB_Iterator& operator++(){}
+		reference operator*() const {return (node->value);}
+		pointer operator->() const {return(&(node->value));}
+		RB_Iterator& operator++(){node = rb_tree_increment(node); return *this;}
+		RB_Iterator operator++(int){RB_Iterator tmp(node); node = rb_tree_increment(node); return(tmp);}
+		RB_Iterator& operator--(){node = rb_tree_decrement(node); return *this;}
+		RB_Iterator operator--(int){RB_Iterator tmp(node); node = rb_tree_decrement(node); return(tmp);}
+		friend bool operator==(const RB_Iterator& lhs, const RB_Iterator& rhs){return lhs.node == rhs.node;}
+		friend bool operator!=(const RB_Iterator& lhs, const RB_Iterator& rhs){return !(lhs.node == rhs.node);}
 	};
 
 	//	RED-BLACK TREE CLASS
@@ -113,13 +113,16 @@ namespace ft
 		typedef Compare													key_compare;
 		typedef Allocator												allocator_type;
 		typedef typename Allocator::template rebind<node_type>::other	node_allocator_type;
+		typedef RB_Iterator												iterator;
 
 		RBTree(const allocator_type& alloc = allocator_type()) : _node_allocator(alloc){
 			_initialize_nil();
 			Root = Nil;
+			_initialize_header();
 		}
 		node_ptr	Root;
 		node_ptr	Nil;
+		node_ptr	Header;
 
 		// node_ptr	grandParent(node_ptr z) {return z->parent->parent;}
 		// node_ptr	aunt(node_ptr z){
@@ -128,6 +131,8 @@ namespace ft
 		// 		return z->GrandParent->right;
 		// 	return z->GrandParent->left;
 		// }
+		iterator	begin(){return iterator(Header->left);}
+		iterator	end(){return iterator(Header);}
 		void	rotate_left(node_ptr x){
 			node_ptr xNewParent = x->right;
 			x->right = xNewParent->left;
@@ -158,82 +163,33 @@ namespace ft
 			xNewParent->right = x;
 			x->parent = xNewParent;
 		}
-		void	insert(const value_type& value){
-			node_ptr z =  _create_node(value);
-			z->left = Nil;
-			z->right = Nil;
-			node_ptr y = NULL;
+		void insert(const value_type& value){
+			if (search(value.first) != Nil)
+				return ;
+			_insert(value);
+			_update_header();
+			_update_root_parent();
+		}
+		node_ptr	search(const key_type& key){
 			node_ptr x = Root;
-
-			while (x != Nil)
+			while (x != Nil && x->key != key)
 			{
-				y = x;
-				if (z->key < x->key)
+				if (key < x->key)
 					x = x->left;
 				else
 					x = x->right;
 			}
-			z->parent = y;
-			if (y == NULL)
-				Root = z;
-			else if (z->key < y->key)
-				y->left = z;
-			else
-				y->right = z;
-			insert_fix(z);
+			return (x);
 		}
-		void insertFix(node_ptr z) {
-			node_ptr y;
-			while (z->parent->color == RED)
-			{
-				if (z->parent == z->parent->parent->left)
-				{
-					y = z->parent->parent->right;
-					if (y->color == RED)
-					{
-						z->parent->color = BLACK;
-						y->color = BLACK;
-						z->parent->parent->color = RED;
-						z = z->parent->parent;
-					}
-					else
-					{
-						if (z == z->parent->right)
-						{
-							z = z->parent;
-							rotate_left(z);
-						}
-						z->parent->color = BLACK;
-						z->parent->parent->color = RED;
-						rotate_right(z->parent->parent);
-					}
-				}
-				else
-				{
-					y = z->parent->parent->left;
-					if (y->color == RED)
-					{
-						z->parent->color = BLACK;
-						y->color = BLACK;
-						z->parent->parent->color = RED;
-						z = z->parent->parent;
-					}
-					else
-					{
-						if (z == z->parent->left)
-						{
-							z = z->parent;
-							rotate_right(z);
-						}
-						z->parent->color = BLACK;
-						z->parent->parent->color = RED;
-						rotate_left(z->parent->parent);
-					}
-				}
-				if (z == Root)
-					break;
-			}
-			Root->color = BLACK;
+		node_ptr	minimum(node_ptr x){
+			while (x->left != Nil)
+				x = x->left;
+			return (x);
+		}
+		node_ptr	maximum(node_ptr x){
+			while (x->right != Nil)
+				x = x->right;
+			return x;
 		}
 		void	transplant(node_ptr u, node_ptr v){
 			if (u->parent == NULL)
@@ -351,22 +307,6 @@ namespace ft
 			}
 			x->color = BLACK;
 		}
-		node_ptr	search(const key_type& key){
-			node_ptr x = Root;
-			while (x != Nil && x->key != key)
-			{
-				if (key < x->key)
-					x = x->left;
-				else
-					x = x->right;
-			}
-			return (x);
-		}
-		node_ptr	minimum(node_ptr x){
-			while (x->left != Nil)
-				x = x->left;
-			return (x);
-		}
 	private:
 		node_allocator_type	_node_allocator;
 
@@ -376,10 +316,107 @@ namespace ft
 			Nil->parent = NULL;
 		}
 
+		void	_initialize_header(){
+			Header = _create_node();
+			Header->color = BLACK;
+			Header->right = Header;
+			Header->left = Header;
+			Header->parent = NULL;
+		}
+
 		node_ptr	_create_node(const value_type& value = value_type()){
 			node_ptr node = _node_allocator.allocate(1);
-			_node_allocator.construct(node, value);// if doesnt compile node_type(value)
+			_node_allocator.construct(node, node_type(value));// if doesnt compile node_type(value)
 			return (node);
+		}
+
+		void	_insert(const value_type& value){
+			node_ptr z =  _create_node(value);
+			z->left = Nil;
+			z->right = Nil;
+			node_ptr y = NULL;
+			node_ptr x = Root;
+
+			while (x != Nil)
+			{
+				y = x;
+				if (z->key < x->key)
+					x = x->left;
+				else
+					x = x->right;
+			}
+			z->parent = y;
+			if (y == NULL)
+				Root = z;
+			else if (z->key < y->key)
+				y->left = z;
+			else
+				y->right = z;
+			insert_fix(z);
+		}
+		void _insert_fix(node_ptr z) {
+			node_ptr y;
+			// if segfault its because when z is root (at first insert) z->parent->color should segfault
+			while (z->parent->color == RED)
+			{
+				if (z->parent == z->parent->parent->left)
+				{
+					y = z->parent->parent->right;
+					if (y->color == RED)
+					{
+						z->parent->color = BLACK;
+						y->color = BLACK;
+						z->parent->parent->color = RED;
+						z = z->parent->parent;
+					}
+					else
+					{
+						if (z == z->parent->right)
+						{
+							z = z->parent;
+							rotate_left(z);
+						}
+						z->parent->color = BLACK;
+						z->parent->parent->color = RED;
+						rotate_right(z->parent->parent);
+					}
+				}
+				else
+				{
+					y = z->parent->parent->left;
+					if (y->color == RED)
+					{
+						z->parent->color = BLACK;
+						y->color = BLACK;
+						z->parent->parent->color = RED;
+						z = z->parent->parent;
+					}
+					else
+					{
+						if (z == z->parent->left)
+						{
+							z = z->parent;
+							rotate_right(z);
+						}
+						z->parent->color = BLACK;
+						z->parent->parent->color = RED;
+						rotate_left(z->parent->parent);
+					}
+				}
+				if (z == Root)
+					break;
+			}
+			Root->color = BLACK;
+		}
+
+		void	_update_header(){
+			Header->parent = Root;
+			Header->left = minimum(Root);
+			Header->right = maximum(Root);
+		}
+
+		void	_update_root_parent(){
+			Root->parent = Header;
 		}
 	};
 }
--- test2.cpp
+++ test2.cpp
@@ -12,19 +12,29 @@
 #include "utility.hpp"
 #include "vector.hpp"
 #include "stack.hpp"
+#include "RBTree.hpp"
 
 int main()
 {
+	// ft::RBTree<int, std::string> tree;
+	// tree.insert(ft::pair<int, std::string>(1, "one"));
 	ft::stack<int> st;
 	std::map<int, std::string> m;
 	std::map<int, std::string>::iterator it;
-	m.insert(std::pair<int, std::string>(0 ,"aprout"));
-	m.insert(std::pair<int, std::string>(1 ,"one"));
-	m.insert(std::pair<int, std::string>(2 ,"two"));
+	// m.insert(std::pair<int, std::string>(0 ,"aprout"));
+	// m.insert(std::pair<int, std::string>(1 ,"one"));
+	// m.insert(std::pair<int, std::string>(2 ,"two"));
+	it = m.end();
+	std::cout << "header = " << it._M_node << std::endl;
+	std::cout << "header->right = " << it._M_node->_M_right << std::endl;
+	std::cout << "header->left = " << it._M_node->_M_left << std::endl;
+	std::cout << "header->parent = " << it._M_node->_M_parent << std::endl;
 	it = m.begin();
-	--it;
-	--it;
-	std::cout << it->second << std::endl;
+	std::cout << "begin = " << it._M_node << std::endl;
+	// --it;
+	// std::cout << "begin - 1 = " << it->second << std::endl;
+	// --it;
+	// std::cout << "begin - 2 = " << it->second << std::endl;
 
 	// std::cout << "address = " << std::addressof(*it) << " - value = " << sizeof(*it) << std::endl;
 	// m.insert(std::pair<int, std::string>(1 ,"btoto"));
